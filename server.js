36dd4a10adb7ff6ab2c4b79a2126e7fcd093b579
AhmadRivaiy <42648525+AhmadRivaiy@users.noreply.github.com>
2023-05-20 15:21:19 +0700

    add-cors

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: '0.0.0.0',
+        "routes": {
+            "cors": {
+                "origin": ["http://localhost:3000", "http://ahmadr901-fe-cicd.s3-website-ap-southeast-2.amazonaws.com"],
+                "headers": ["Accept", "Content-Type"],
+                "additionalHeaders": ["X-Requested-With"]
+            }
+        }
     });
 
     await server.start();
